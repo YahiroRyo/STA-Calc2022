a14445ea75491dfa3c633377668cbb020ff19f44
YahiroRyo <honakafn@gmail.com>
2022-06-17 17:09:02 +0900

    ユーザー作成画面実装

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,13 +4,15 @@ import type { Component } from 'solid-js';
 import styles from './App.module.scss';
 import Calc from './components/pages/Calc';
 import Login from './components/pages/Login';
+import UserCreate from './components/pages/UserCreate';
 
 const App: Component = () => {
   return (
     <div class={styles.app}>
       <Routes>
         <Route path='/' element={<Calc />} />
-        <Route path='/login' element={<Login />} />
+        <Route path='/users/login' element={<Login />} />
+        <Route path='/users/create' element={<UserCreate />} />
       </Routes>
     </div>
   );
