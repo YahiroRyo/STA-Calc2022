409b4407d26c3241021421a1a5883cf0aec918d4
YahiroRyo <honakafn@gmail.com>
2022-06-18 19:38:20 +0900

    nginxでproxyを通した

--- api/config/cors.php
+++ api/config/cors.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'paths' => ['/users/*', 'api/*', 'sanctum/csrf-cookie'],
+    'paths' => ['users/*', 'api/*', 'sanctum/csrf-cookie'],
 
     'allowed_methods' => ['*'],
 
